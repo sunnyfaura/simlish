737bc7e9acaf0d6a93fccdb7dfb27bb43d8cde95
beadanseco <beatrice_danseco@yahoo.com>
2014-02-19 04:03:45 +0800

    update parser

--- src/SimlishParser.java
+++ src/SimlishParser.java
@@ -3,12 +3,20 @@ import java.util.LinkedList;
 
 public class SimlishParser {
 	LinkedList<Token> simlish;
+	Token first;
 
-	public void setTokens(LinkedList<Token> list) {
+	public void nextToken() {
+		simlish.pop();
+		if(simlish.isEmpty())
+			first = new simlish(simlish.EPSILON, "", -1);
+		else
+			first = simlish.getFirst();		
+	}
+
+	public void parse(LinkedList<Token> list) {
 		simlish = list;
+		first = simlish.getFirst();
+		//call start of grammarz
 	}
-
-	public void parse() {
-		
-	}
+	
 }
