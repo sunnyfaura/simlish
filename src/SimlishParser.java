89bae8c97ab93158f2b1cba26ffc94c15a8dd762
beadanseco <beatrice_danseco@yahoo.com>
2014-02-20 19:12:00 +0800

    updated parser
    marami pang kulang pero may parang concern ako regarding sa grammar ulit
    :)) di ko maarticulate now :3 needs  sleep sorry will wake up laters :3

--- src/SimlishParser.java
+++ src/SimlishParser.java
@@ -11,12 +11,109 @@ public class SimlishParser {
 			first = new simlish(simlish.EPSILON, "", -1);
 		else
 			first = simlish.getFirst();		
+		program();
 	}
 
 	public void parse(LinkedList<Token> list) {
 		simlish = list;
 		first = simlish.getFirst();
 		//call start of grammarz
+	}
+
+	//grammars
+	//PROGRAM
+	public void program()
+	{
+		if(first.equals("program_init"))
+		{
+			nextToken();
+			init();
+			program_main();
+			if(first.equals("program_terminator")) //unsure if it's supposed to be here
+			{
+				
+			}
+			else
+			{
+				System.out.println("ERROR: Program wasn't terminated properly.");
+			}
+		}
+		else
+		{
+			System.out.println("ERROR: Program wasn't started properly.");
+		}
+	}
+	public void init()
+	{
+		var();
+		arr();
+	}
+	public void program_main()
+	{
+		
+	}
+	
+	public void var()
+	{
+		variable_declaration();
+		//OR EPSILON
+	}
+	public void arr()
+	{	
+		array_declaration();
+		//OR EPSILON
+	}
+	public void variable_declaration()
+	{
+		if(first.equals("var_init"))
+		{
+			nextToken();
+			if(first.equals(":"))
+			{
+				nextToken();
+				variable();
+				if(first.equals("block_terminator"))
+				{
+					System.out.println("ERROR: CREATE A SIM wasn't terminated properly.");
+				}
+				else
+				{
+					
+				}
+			}
+		}
+		//create an else-if for if epsilon
+		//create an else it for array
+		else
+		{
+			System.out.println("ERROR: CREATE A SIM wasn't started properly.");
+		}
+	}
+	public void array_declaration()
+	{
+
+	}
+
+	//VARIABLE DECLARATION
+	public void variable()
+	{
+		T();
+		variable();
+	}
+	public void T()
+	{
+		U();
+		V();
+		//if for period
+	}
+	public void U()
+	{
+		//if identifier
+			//call data_type();
+	}
+	public void V()
+	{
+		
 	}
 	
 }
