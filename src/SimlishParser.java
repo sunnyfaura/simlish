d9d55d943dae5de01805b8c4f7e64423678572d9
Christine Tero <christine.tero@gmail.com>
2014-02-22 17:53:35 +0800

    WIP assignments

--- src/SimlishParser.java
+++ src/SimlishParser.java
@@ -5,6 +5,7 @@ public class SimlishParser {
 	LinkedList<Token> lookupTable;
 	LinkedList<Symbol> symbolTable;
 	Token first;
+	Symbol top;
 
 	public void nextToken() {
 		lookupTable.pop();
@@ -13,7 +14,7 @@ public class SimlishParser {
 		else
 			first = lookupTable.getFirst();	
 	}
-	
+
 	public void parse(LinkedList<Token> list) {
 		lookupTable = list;
 		symbolTable = new LinkedList<Symbol>();
@@ -86,7 +87,11 @@ public class SimlishParser {
 		nextToken();
 		String name = "";
 		if ( first.token.equals("IDENTIFIER") ) {
-			name = first.lexeme;
+			if ( symbolTable.contains(name) ) {
+				throw new ParserException( first.lexeme+" already exists." );
+			} else {
+				name = first.lexeme;
+			}
 		}
 		nextToken();
 		if( first.token.equals("LPAREN") ) {
@@ -110,7 +115,7 @@ public class SimlishParser {
 	}
 
 	
-	public void V() {
+	public void V() {		
 		nextToken();
 		if( first.token.equals("STRING_ASGN") ) {
 			System.out.println("Ok doing a String assignment.");
@@ -127,18 +132,22 @@ public class SimlishParser {
 		nextToken();
 		if( first.token.equals("DQUOTE") ) {
 			assignment();
-			if ( first.token.equals("DQUOTE") ) {
-				System.out.println("Correct syntax for assignment");
-			} else {
-				throw new ParserException("Missing starting DQUOTE for an assignment operation.");
-			}
 		} else {
 			throw new ParserException("Missing terminating DQUOTE for an assignment operation.");
 		}
 	}
 	
+	String value = "";
+	
 	public void assignment() {
-		
+		//I'm trying to make the thing get everything inside the DQUOTES and insert it as a new Symbol
+		top = symbolTable.peek();
+		nextToken();
+		if ( first.token.equals("DQUOTE") ) {
+			System.out.println("Correct syntax for assignment");
+		} else {
+			throw new ParserException("Missing starting DQUOTE for an assignment operation.");
+		}
 	}
 	
 	public void array_declaration() {
