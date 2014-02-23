9206cf58f4a00a968f7e91a195464a40f3bb2d3e
beadanseco <beatrice_danseco@yahoo.com>
2014-02-23 16:04:12 +0800

    element class

---
+++ src/Element.java
@@ -0,0 +1,36 @@
+
+public class Element {
+	public String dataType;
+	public String human;
+	public String gender;
+	public int pet;
+	public float supernatural;
+	public String getHuman() {
+		return human;
+	}
+	public void setHuman(String human) {
+		this.human = human;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getPet() {
+		return pet;
+	}
+	public void setPet(int pet) {
+		this.pet = pet;
+	}
+	public float getSupernatural() {
+		return supernatural;
+	}
+	public void setSupernatural(float supernatural) {
+		this.supernatural = supernatural;
+	}
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
+	}
+	
+}
